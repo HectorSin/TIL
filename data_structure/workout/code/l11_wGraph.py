b4f16a2a91f96571b866d084a12d0394f5a0de78
Hector <kkang15634@ajou.ac.kr>
2023-06-20 01:13:37 +0900

    changes

--- data_structure/workout/code/l11_wGraph.py
+++ data_structure/workout/code/l11_wGraph.py
@@ -36,6 +36,38 @@ def printAllEdges(vlist, W):
 
 ### 인접 리스트를 이용한 표현
 
+# 가중치의 총 합을 구하는 함수
+def weightSum(graph):
+    sum = 0
+    # 그래프의 모든 정점 v에 대해: 'A', 'B', ...
+    for v in graph:
+        # v의 모든 간선 e에 대해: ('B', 29), ...
+        for e in graph[v]:
+            # sum에 추가
+            sum += e[1]
+    # 하나의 간선이 두 번 더해지므로 2로 나눔
+    return sum//2
+
+# 가중치의 총 합을 구하는 함수
+def weightSum(graph):
+    sum = 0
+    # 그래프의 모든 정점 v에 대해: 'A', 'B', ...
+    for v in graph:
+        # v의 모든 간선 e에 대해: ('B', 29), ...
+        for e in graph[v]:
+            # sum에 추가
+            sum += e[1]
+    # 하나의 간선이 두 번 더해지므로 2로 나눔
+    return sum//2
+
+# 모든 간선을 출력하는 함수
+def printAllEdges(graph):
+    # 그래프의 모든 정점 v에 대해: 'A', 'B', ...
+    for v in graph:
+        # v의 모든 간선 e에 대해: ('B', 29), ...
+        for e in graph[v]:
+            print("(%s, %s, %d)"%(v,e[0],e[1]), end=' ')
+
 ### union-find 구현
 
 ### Kruskal 알고리즘의 구현
